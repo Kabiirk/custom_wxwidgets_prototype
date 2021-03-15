2032f9389bfd6aaecdb86f56aacbd7d8a6be49ec
Kabiirk <kabiirk@gmail.com>
2021-03-15 17:55:00 +0530

    edited test.py

--- test.py
+++ test.py
@@ -5,95 +5,120 @@
 # https://stackoverflow.com/questions/6745463/exit-frame-and-panel-from-panel-method -> Close parent frame from a button in the panel
 # https://www.programcreek.com/python/example/4469/wx.ComboBox a good wesite for examples
 # https://zetcode.com/gui/wxwidgets/widgetsII/ -> for listboxes
-
-
+# https://www.programcreek.com/python/example/97212/wx.ColourDialog -> Examples of colorpickerctrl 
 import wx
 
+
+template = [['A','B','C'], ['D','E'], ['F'], ['G','H']]
+
 class MyPanel(wx.Panel):
-    """
-    class MyPanel creates a panel with 2 comboboxes and more, inherits wx.Panel
-    (putting your components/widgets on a panel gives additional versatility)
-    """
-    def __init__(self, parent, id):
-        # no pos and size given, so panel defaults to fill the parent frame
-        wx.Panel.__init__(self, parent, id)
-        self.SetBackgroundColour((255,228,196))  # bisque
+    """"""
+    #----------------------------------------------------------------------
+    def __init__(self, parent):
+        """Constructor"""
+        wx.Panel.__init__(self, parent)
+        self.number_of_buttons = 0
+        self.templates = [['A','B','C'], ['D','E'], ['F'], ['G','H']]
+        
+        self.frame = parent
+        
+        self.mainSizer = wx.BoxSizer(wx.VERTICAL)
+        widgetSizer = wx.BoxSizer(wx.VERTICAL)
+        bottomSizer = wx.BoxSizer(wx.HORIZONTAL)
 
-         # no size given, so the text determines the needed label size       
-        wx.StaticText(self, -1, "convert from:", (10, 10))
-        # create a combo box to select units of measurement to convert from
-        self.combo1 = wx.ComboBox(self, -1, value=areaList[0], pos=wx.Point(10, 30),
-            size=wx.Size(120, 150), choices=areaList)
-        # optional tooltip
-        self.combo1.SetToolTip(wx.ToolTip("select unit from dropdown-list"))
-        
-        wx.StaticText(self, -1, "convert to:", pos=wx.Point(150, 10))
-        # create a combo box to select units of measurement to convert to
-        self.combo2 = wx.ComboBox(self, -1, value=areaList[0], pos=wx.Point(150, 30),
-            size=wx.Size(120, 150), choices=areaList)
-        
-        wx.StaticText(self, -1, "value to convert:", pos=wx.Point(10, 70))
-        self.edit1 = wx.TextCtrl(self, -1, value="1", pos=wx.Point(10, 90), size=wx.Size(175, 25))
-        self.edit1.SetBackgroundColour((255,255,197))  # suds yellow
-        
-        self.button1 = wx.Button(self, -1, label="Do the Conversion ...",
-            pos=wx.Point(10, 130), size=wx.Size(175, 28))
-        # respond to button click event
-        self.button1.Bind(wx.EVT_BUTTON, self.button1Click, self.button1)
-        
-        wx.StaticText(self, -1, "result:", (10, 170))
-        self.edit2 = wx.TextCtrl(self, -1, value="", pos=wx.Point(10, 190), size=wx.Size(350, 25))
-        self.edit2.SetBackgroundColour((217,255,219))  # vegaseat green
-       
-    def button1Click(self,event):
-        """Conversion button has been clicked"""
-        unit1 = self.combo1.GetValue()
-        unit2 = self.combo2.GetValue()
-        x = float(self.edit1.GetValue())
-        y = convertArea(x, unit1, unit2)
-        if y < 0.001:
-            str1 = "%f %s = %0.12f %s" % (x, unit1, y, unit2)  # very small y
-        elif y > 1000:
-            str1 = "%f %s = %0.3f %s" % (x, unit1, y, unit2)   # very large y
-        else:
-            str1 = "%f %s = %f %s" % (x, unit1, y, unit2)      # 6 decimals is default
-        self.edit2.SetValue(str1)
-        
+        # WIDGETSIZER
+        for i in range(0,len(self.templates)):
+            static_text = wx.StaticText(self, -1, style = wx.ALIGN_LEFT)
+            static_text.SetLabel("Plot"+str(i+1))
+            widgetSizer.Add(static_text, 0, wx.CENTER|wx.ALL, 5)
+            
+            for j in range(0,len(self.templates[i])):
+                hsizer = wx.BoxSizer(wx.HORIZONTAL)
+                color_picker = wx.ColourPickerCtrl(self, id=wx.ID_ANY, colour=wx.BLACK, style=wx.CLRP_DEFAULT_STYLE, validator=wx.DefaultValidator, name=self.templates[i][j])
+                static_text = wx.StaticText(self, -1, style = wx.ALIGN_LEFT)
+                static_text.SetLabel("Color of "+self.templates[i][j])
+                hsizer.Add(static_text, 0, wx.EXPAND | wx.RIGHT, 5)
+                hsizer.Add(color_picker, 0, wx.EXPAND | wx.LEFT, 5)
 
-def convertArea(x, unit1, unit2):
-    """convert area x of unit1 to area of unit2 and return area, on error return False"""
-    if (unit1 in areaD) and (unit2 in areaD):
-        factor1 = areaD[unit1]
-        factor2 = areaD[unit2]
-        return factor2*x/factor1
-    else:
-        return False
+                widgetSizer.Add(hsizer, flag=wx.ALL|wx.ALIGN_CENTER, border=5)
+        # BOTTOM SIZER
+        closeBtn = wx.Button(self, label="Cancel")
+        closeBtn.Bind(wx.EVT_BUTTON, self.onClose)
+        bottomSizer.Add(closeBtn,  0, wx.CENTER|wx.ALL, 5)
+
+        self.plotBtn = wx.Button(self, label="Plot")
+        self.plotBtn.Bind(wx.EVT_BUTTON, self.onPlot)
+        bottomSizer.Add(self.plotBtn,  0, wx.CENTER|wx.ALL, 5)
+        
+        self.mainSizer.Add(widgetSizer, 0, wx.CENTER|wx.ALL, 10)
+        self.mainSizer.Add(bottomSizer, 0, wx.CENTER|wx.ALL, 10)
+        
+        self.SetSizer(self.mainSizer)
+        
+    #----------------------------------------------------------------------
+    def onAddWidget(self, event):
+        self.number_of_buttons += 2
+        
+        text_box_name = "properties"+str(self.number_of_buttons)
+
+        #self.choose_property = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, sampleList, 0 )
+        self.choose_property = wx.ListBox(self, style=wx.LB_MULTIPLE, choices=self.sampleList)
+        # self.choose_property = wx.TextCtrl(self, -1, "", name=text_box_name)
+        # self.choose_property.SetHint('Add Properties, separated by comma')
+
+        self.widgetSizer.Add(self.choose_property, 0, wx.ALL, 5)
+        self.frame.fSizer.Layout()
+        self.frame.Fit()
     
-#create an empty dictionary
-areaD = {}
-# populate dictionary with units and conversion factors relative to sqmeter = 1.0
-# this minimizes the total number of conversion factors
-areaD['sqmeter']      = 1.0
-areaD['sqmillimeter'] = 1000000.0
-areaD['sqcentimeter'] = 10000.0
-areaD['sqkilometer']  = 0.000001
-areaD['hectare']      = 0.0001
-areaD['sqinch']       = 1550.003
-areaD['sqfoot']       = 10.76391
-areaD['sqyard']       = 1.19599
-areaD['acre']         = 0.0002471054
-areaD['sqmile']       = 0.0000003861022
+    #----------------------------------------------------------------------
+    def onRemoveWidget(self, event):
+        if self.widgetSizer.GetChildren():
+            sizer_item = self.widgetSizer.GetItem(self.number_of_buttons-1)
+            widget = sizer_item.GetWindow()
+            self.widgetSizer.Hide(widget)
+            widget.Destroy()
+            self.number_of_buttons -= 1
+            self.frame.fSizer.Layout()
+            self.frame.Fit()
 
-# create a sorted list for the combo boxes
-areaList = sorted(areaD.keys())
+        else:
+            mssg_box = wx.MessageBox("No Plots to remove !", "Message" ,wx.OK | wx.ICON_INFORMATION)  
 
+    #----------------------------------------------------------------------
+    def onClose(self, event):
+        frame = self.GetParent()
+        frame.Close()
 
-app = wx.PySimpleApp()
-# create a window/frame, no parent, -1 is default ID, title, size
-frame = wx.Frame(None, -1, "Convert Area ...", size = (400, 300))
-# call the derived class, -1 is default ID, can also use wx.ID_ANY
-MyPanel(frame,-1)
-# show the frame
-frame.Show(True)
-# start the event loop
-app.MainLoop()
+    #----------------------------------------------------------------------
+    def onPlot(self, event):
+        all_colors = []
+
+        ColorPicker_objects = [widget for widget in self.GetChildren() ]#if isinstance(widget, wx.ColourPickerCtrl)]
+
+        # for ColorPicker_object in ColorPicker_objects:
+        #     all_colors.append(ColorPicker_objects.GetColour())
+        print(ColorPicker_objects)
+
+    def setTemplate(self, template_list):
+        self.templates = template_list
+            
+                
+########################################################################
+class MyFrame(wx.Frame):
+    """"""
+    #----------------------------------------------------------------------
+    def __init__(self):
+        """Constructor"""
+        wx.Frame.__init__(self, parent=None, title="Add / Remove Buttons")
+        self.fSizer = wx.BoxSizer(wx.VERTICAL)
+        panel = MyPanel(self)
+        self.fSizer.Add(panel, 1, wx.EXPAND)
+        self.SetSizer(self.fSizer)
+        self.Fit()
+        self.Show()
+        
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = MyFrame()
+    app.MainLoop()
